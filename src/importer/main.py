9b388d5d2d17448a2c6669981c865cd5c83d1141
Luís Teófilo <luisfgteofilo@gmail.com>
2022-12-13 16:25:49 +0000

    use watchdog in importer

--- src/importer/main.py
+++ src/importer/main.py
@@ -2,54 +2,75 @@ import os
 import time
 import uuid
 
+import os
+import watchdog
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
 from utils.to_xml_converter import CSVtoXMLConverter
 
 CSV_INPUT_PATH = "/csv"
 XML_OUTPUT_PATH = "/shared/output"
 
+
 def get_converted_files():
-    #!TODO: you should retrieve from the database the files that were already converted before
+    # !TODO: you should retrieve from the database the files that were already converted before
     return []
 
 def get_csv_files_in_input_folder():
     return [os.path.join(dp, f) for dp, dn, filenames in os.walk(CSV_INPUT_PATH) for f in filenames if
-                     os.path.splitext(f)[1] == '.csv']
+            os.path.splitext(f)[1] == '.csv']
+
 
 def generate_unique_file_name():
     return f"/shared/output/{str(uuid.uuid4())}.xml"
+
+
 def convert_csv_to_xml(in_path, out_path):
     converter = CSVtoXMLConverter(in_path)
     file = open(out_path, "w")
     file.write(converter.to_xml_str())
 
+
+class CSVHandler(FileSystemEventHandler):
+    def __init__(self):
+        self._converted_files = get_converted_files()
+
+    def convert_csv(self, csv_path):
+        # here we avoid converting the same file again
+        # !TODO: check converted files in the database
+        if csv_path in self._converted_files:
+            return
+
+        print(f"new file to convert: '{csv_path}'")
+
+        # we generate an unique file name for the XML file
+        xml_path = generate_unique_file_name()
+
+        # we do the conversion
+        # !TODO: once the conversion is done, we should updated the converted_documents tables
+        convert_csv_to_xml(csv_path, xml_path)
+        print(f"new xml file generated: '{xml_path}'")
+
+        # !TODO: we should store the XML document into the imported_documents table
+        # !FIXME: instead of updating the local cache for converted files, we should reload them from the database
+        # !FIXME: in the next iteration
+        self._converted_files.append(csv_path)
+
+    def on_created(self, event):
+        if not event.is_directory and event.src_path.endswith(".csv"):
+            self.convert_csv(event.src_path)
+
+
 if __name__ == "__main__":
-    converted_files = get_converted_files()
-
-    while True:
-        print("looking for new files...")
-        csv_files = get_csv_files_in_input_folder()
-
-        for csv_path in csv_files:
-
-            # here we avoid converting the same file again
-            if csv_path in converted_files:
-                continue
-
-            print(f"new file to convert: '{csv_path}'")
-
-            # we generate an unique file name for the XML file
-            xml_path = generate_unique_file_name()
-
-            # we do the conversion
-            convert_csv_to_xml(csv_path, xml_path)
-
-            print(f"new xml file generated: '{xml_path}'")
-
-            #!TODO: we store the XML into the imported_documents table
-
-            #!FIXME: instead of updating the local cache for converted files, we should reload them from the database
-            #!FIXME: in the next iteration
-            converted_files.append(csv_path)
-
-        # hold execution for 60 seconds
-        time.sleep(60)
+    print("main")
+    observer = Observer()
+    event_handler = CSVHandler()
+    observer.schedule(event_handler, path=CSV_INPUT_PATH, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()

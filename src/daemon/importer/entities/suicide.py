1f42137e287caf7558272691fe513100729ce149
Luís Filipe Malheiro <lmalheiro@ipvc.pt>
2022-12-30 14:58:30 +0000

    update entities

--- src/daemon/importer/entities/suicide.py
+++ src/daemon/importer/entities/suicide.py
@@ -7,3 +7,23 @@ class Suicide:
         Country.counter += 1
         self._id = Country.counter
         self._name = name
+
+    def __init__(self, id, idcountry, year, sex, minage, maxage, tax, population_no, suicides_no,
+                 generation, gdp_for_year, hdi_for_year, gdp_per_capita):
+        self.id = id
+        self.idcountry = idcountry
+        self.year = year
+        self.sex = sex
+        self.minage = minage
+        self.maxage = maxage
+        self.tax = tax
+        self.population_no = population_no
+        self.suicides_no = suicides_no
+        self.generation = generation
+        self.gdp_for_year = gdp_for_year
+        self.hdi_for_year = hdi_for_year
+        self.gdp_per_capita = gdp_per_capita
+
+
+
+

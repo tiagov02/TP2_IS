833a4861b490bca956d1f968f88415deb9e40a6e
Tiago Viana <tiagoviana.1003@gmail.com>
2023-01-14 03:54:09 +0000

    finish the endpoints to update and create countries

--- src/api/entities/main.py
+++ src/api/entities/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,abort
 from entities.suicide import Suicide
 from entities.country import Country
 import psycopg2
@@ -116,13 +116,13 @@ def get_country(id:str):
     cursor = connection.cursor()
     cursor.execute(f"SELECT * from countries WHERE id=\'{id}\' ")
     first = cursor.fetchone()
-    return Country(
+    return jsonify(Country(
         id=first[0],
         name=first[1],
         geom=first[2],
         created_on=first[3],
         updated_on=first[4]
-    ).__dict__
+    ).__dict__),200
 
 @app.route('/api/countries/to_update', methods=['GET'])
 def get_100_countries_to_update():
@@ -143,6 +143,49 @@ def get_100_countries_to_update():
             updated_on=result[4]
         ))
     return jsonify([country.__dict__ for country in countries])
+
+@app.route('/api/countries/create',methods=['POST'])
+def create_country():
+    data = request.get_json()
+    name = data['name']
+
+    connection = psycopg2.connect(user="is",
+                                  password="is",
+                                  host="db-rel",
+                                  database="is")
+
+    cursor = connection.cursor()
+    cursor.execute(f"insert into countries (name) values (\'{name}\') RETURNING id")
+    new_id = cursor.fetchone()[0]
+    if new_id is not None:
+        return get_country(new_id)
+    else:
+        abort(500)
+
+@app.route('/api/countries/update',methods=['POST'])
+def update_country():
+    data = request.get_json()
+    id = data['id']
+    name = data['name']
+    lat = data['lat']
+    lon = data['lon']
+
+    connection = psycopg2.connect(user="is",
+                                  password="is",
+                                  host="db-rel",
+                                  database="is")
+    cursor = connection.cursor()
+    cursor.execute(f"UPDATE countries SET name=\'{name}\',geom = ST_MakePoint({lon}, {lat}) WHERE id=\'{id}\'")
+    cursor.execute(f"SELECT * from countries WHERE id=\'{id}\' ")
+    first = cursor.fetchone()
+    return jsonify(Country(
+        id=first[0],
+        name=first[1],
+        geom=first[2],
+        created_on=first[3],
+        updated_on=first[4]
+    ).__dict__),201
+
 
 
 if __name__ == '__main__':

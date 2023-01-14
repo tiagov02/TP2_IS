298864ca0b00bd4a51ed033d720c2391e42f4592
Tiago Viana <tiagoviana.1003@gmail.com>
2023-01-14 19:08:44 +0000

    api entities finished

--- src/daemon/importer/utils/to_xml_converter.py
+++ src/daemon/importer/utils/to_xml_converter.py
@@ -1,62 +1,45 @@
 import csv
 import xml.dom.minidom as md
 import xml.etree.ElementTree as ET
-
-from utils.reader import CSVReader
-from entities.country import Country
-from entities.team import Team
 #from entities.player import Player
-
+import pandas as pd
 
 class CSVtoXMLConverter:
 
     def __init__(self, path):
-        self._reader = CSVReader(path)
+        self._reader = pd.read_csv(path)
 
     def to_xml(self):
-        # read countries
-        countries = self._reader.read_entities(
-            attr="nationality",
-            builder=lambda row: Country(row["nationality"])
-        )
+        dataset = self._reader
+        root = ET.Element('SUICIDES')
+        for year, df_group in dataset.groupby('year'):
+            i = 0
+            years = ET.SubElement(root, 'YEAR', {'code': str(year)})
+            for country, df_group_country in df_group.groupby('country'):
+                countys = ET.SubElement(years, 'COUNTRY',{'name': str(country)})
+                for item in df_group_country.iterrows():
+                    aux = item[1].T
+                    minAge = None
+                    maxAge = None
+                    if (aux['age'] == "75+ years"):
+                        minAge = "75"
+                        maxAge = "MAX"
+                    else:
+                        auxStr = str(aux['age']).split("-")
+                        auxMax = str(auxStr[1]).split()
+                        minAge = auxStr[0]
+                        maxAge = auxMax[0]
+                    suicides = ET.SubElement(countys, 'SUICIDE',
+                                             {'sex': str(aux['sex']), 'minAge': str(minAge), 'maxAge': str(maxAge),
+                                              'tax': str(aux['suicides/100k pop']),
+                                              'population_no': str(aux['population']),
+                                              'suicides_no': str(aux['suicides_no']),
+                                              'generation': str(aux['generation']),
+                                              'gdp_for_year': str(aux[' gdp_for_year ($) ']),
+                                              'hdi_for_year': str(aux['HDI for year']),
+                                              'gdp_per_capita': str(aux['gdp_per_capita ($)'])})
 
-        # read teams
-        teams = self._reader.read_entities(
-            attr="Current Club",
-            builder=lambda row: Team(row["Current Club"])
-        )
-
-        # read players
-
-        def after_creating_player(player, row):
-            # add the player to the appropriate team
-            teams[row["Current Club"]].add_player(player)
-
-        self._reader.read_entities(
-            attr="full_name",
-            builder=lambda row: Player(
-                name=row["full_name"],
-                age=row["age"],
-                country=countries[row["nationality"]]
-            ),
-            after_create=after_creating_player
-        )
-
-        # generate the final xml
-        root_el = ET.Element("Football")
-
-        teams_el = ET.Element("Teams")
-        for team in teams.values():
-            teams_el.append(team.to_xml())
-
-        countries_el = ET.Element("Countries")
-        for country in countries.values():
-            countries_el.append(country.to_xml())
-
-        root_el.append(teams_el)
-        root_el.append(countries_el)
-
-        return root_el
+        return root
 
     def to_xml_str(self):
         xml_str = ET.tostring(self.to_xml(), encoding='utf8', method='xml').decode()

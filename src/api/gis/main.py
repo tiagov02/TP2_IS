4c51405b0732fa502a302312b9b4a2206b646b9a
Tiago Viana <tiagoviana.1003@gmail.com>
2023-01-16 03:59:12 +0000

    update api-gis

--- src/api/gis/main.py
+++ src/api/gis/main.py
@@ -1,6 +1,7 @@
 import sys
 
-from flask import Flask, request
+import psycopg2
+from flask import Flask, request, jsonify
 
 PORT = int(sys.argv[1]) if len(sys.argv) >= 2 else 9000
 
@@ -29,7 +30,29 @@ def get_markers():
             }
         }
     ]
+#This returns all
+@app.route('/api/all')
+def get_all():
+    connection = psycopg2.connect(user="is",
+                                  password="is",
+                                  host="db-rel",
+                                  database="is")
 
+    cursor = connection.cursor()
+    cursor.execute(f"WITH suicide_country as( "
+                   f"select SUM(s.suicides_no) as suicides_no, AVG(s.tax) as med_tax, c.name as country , c.geom as geom "
+                   f" FROM countries c, suicides s "
+                   f"WHERE s.id_country = c.id AND st_makeenvelope(39.50,76.4,39.3,76.6) "
+                   f"GROUP BY s.id_country, c.name, c.geom "
+                   f")"
+                   f"SELECT jsonb_build_object(  "
+                   f"'type', 'feature', "
+                   f"'geometry', ST_AsGeoJSON(geom), "
+                   f"'proprieties', to_jsonb(sc.*) -'id' -'geom' "
+                   f") AS json  "
+                   f"FROM suicide_country sc; ")
+    res = cursor.fetchall()
+    return jsonify([country for country in res])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=PORT)

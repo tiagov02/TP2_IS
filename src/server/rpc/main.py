61448ddb692afeea02627ca837408bd1cf136a36
Tiago Viana <tiagoviana.1003@gmail.com>
2023-01-17 00:51:13 +0000

    update rpc-server

--- src/server/rpc/main.py
+++ src/server/rpc/main.py
@@ -274,33 +274,22 @@ if __name__ == "__main__":
                 cursor = connection.cursor()
                 print(year)
 
-                cursor.execute(
-                    f"with suicides as\t"
-                    f"(select unnest(xpath('//SUICIDES/YEAR[@code=\"{year}\"]/COUNTRY/SUICIDE', xml))\t"
-                    f"as suicide from imported_documents WHERE id={id})\t"
-                    f"SELECT ((xpath('@sex', suicide))[1]::varchar) as sex,\t"
-                    f"(sum((xpath('@suicides_no',suicide))[1]::varchar::numeric)) :: varchar\t"
-                    f"FROM suicides GROUP BY (xpath('@sex', suicide))[1]::varchar")
+                cursor.execute(f"SELECT sex,SUM(suicides_no) from suicides "
+                               f"WHERE year={year} "
+                               f"group by sex; ")
                 for ns in cursor:
                     nSuicides.append(ns)
                 cursor.close()
 
                 cursor = connection.cursor()
-                cursor.execute(f"with suicides as ("
-                               f"select unnest(xpath('//SUICIDES/YEAR[@code=\"{year}\"]/COUNTRY/SUICIDE[@minAge < 15]', xml)) "
-                               f"as suicide from imported_documents WHERE id={id}) "
-                               f"SELECT (sum((xpath('@suicides_no',suicide))[1]::varchar::numeric)) :: varchar "
-                               f"FROM suicides")
+                cursor.execute(f"SELECT SUM(suicides_no) from suicides "
+                               f"WHERE year={year} AND min_age < 15;")
                 for c in cursor:
                     children.append(c)
                 cursor.close()
                 cursor = connection.cursor()
-                cursor.execute(f"with suicides as ("
-                               f"select unnest(xpath('//SUICIDES/YEAR[@code=\"{year}\"]/COUNTRY/SUICIDE[@maxAge =\"MAX\"]', xml)) "
-                               f"as suicide "
-                               f"from imported_documents WHERE id={id}) "
-                               f"SELECT (sum((xpath('@suicides_no',suicide))[1]::varchar::numeric))::varchar "
-                               f"FROM suicides")
+                cursor.execute(f"SELECT SUM(suicides_no) from suicides "
+                               f"WHERE year={year} AND max_age = 0;")
                 for o in cursor:
                     olders.append(o)
             except (Exception, psycopg2.Error) as error:
@@ -310,6 +299,142 @@ if __name__ == "__main__":
                     cursor.close()
                     connection.close()
             return [nSuicides, children, olders]
+
+
+        def orderByCountry(country):
+            nSuicides = []
+            children = []
+            olders = []
+            try:
+                connection = psycopg2.connect(host='db-rel', database='is', user='is', password='is')
+
+                cursor = connection.cursor()
+
+                cursor.execute(
+                    f"SELECT s.sex , SUM(s.suicides_no) from suicides s, countries c "
+                    f"WHERE s.id_country=c.id AND c.name like \'%{country}%\' "
+                    f"Group by s.sex ")
+                for ns in cursor:
+                    nSuicides.append(ns)
+                cursor.close()
+
+                cursor = connection.cursor()
+                cursor.execute(f"SELECT SUM(s.suicides_no) from suicides s, countries c "
+                               f"WHERE s.id_country=c.id AND c.name like \'%{country}%\' AND s.min_age<15 ")
+                for c in cursor:
+                    children.append(c)
+                cursor.close()
+                cursor = connection.cursor()
+                cursor.execute(f"SELECT SUM(s.suicides_no) from suicides s, countries c "
+                               f"WHERE s.id_country=c.id AND c.name like \'%{country}%\' AND s.max_age=0 ")
+                for o in cursor:
+                    olders.append(o)
+            except (Exception, psycopg2.Error) as error:
+                print("Failed to fetch data", error)
+            finally:
+                if connection:
+                    cursor.close()
+                    connection.close()
+            return [nSuicides, children, olders]
+
+        def orderByYarAndCountry(year, country):
+            nSuicides = []
+            children = []
+            olders = []
+            try:
+                connection = psycopg2.connect(host='db-rel', database='is', user='is', password='is')
+
+                cursor = connection.cursor()
+
+                cursor.execute(
+                    f"SELECT s.sex , SUM(s.suicides_no) from suicides s, countries c "
+                    f"WHERE s.id_country=c.id AND c.name like \'%{country}%\' AND s.year={year}"
+                    f"Group by s.sex ")
+                for ns in cursor:
+                    nSuicides.append(ns)
+                cursor.close()
+
+                cursor = connection.cursor()
+                cursor.execute(f"SELECT SUM(s.suicides_no) from suicides s, countries c "
+                               f"WHERE s.id_country=c.id AND c.name like \'%{country}%\' AND s.min_age<15 AND s.year={year}")
+                for c in cursor:
+                    children.append(c)
+                cursor.close()
+                cursor = connection.cursor()
+                cursor.execute(f"SELECT SUM(s.suicides_no) from suicides s, countries c "
+                               f"WHERE s.id_country=c.id AND c.name like \'%{country}%\' AND s.max_age=0 AND s.year={year}")
+                for o in cursor:
+                    olders.append(o)
+            except (Exception, psycopg2.Error) as error:
+                print("Failed to fetch data", error)
+            finally:
+                if connection:
+                    cursor.close()
+                    connection.close()
+            return [nSuicides, children, olders]
+
+
+        def suicidesInRichCountry():
+            res = []
+            try:
+                connection = psycopg2.connect(host='db-xml', database='is', user='is', password='is')
+
+                cursor = connection.cursor()
+
+                cursor.execute("SELECT MAX (id) from imported_documents")
+                id = cursor.fetchone()[0]
+
+                cursor.execute(f"SELECT sex , SUM(suicides_no) from suicides "
+                               f"WHERE gdp_per_capita>18577 "
+                               f"Group by sex ")
+                for d in cursor:
+                    res.append(d)
+            except (Exception, psycopg2.Error) as error:
+                print("Failed to fetch data", error)
+            finally:
+                if connection:
+                    cursor.close()
+                    connection.close()
+            return res
+
+
+        def CountryWithLessandMoreSuicides():
+            res = []
+            try:
+                connection = psycopg2.connect(host='db-xml', database='is', user='is', password='is')
+
+                cursor = connection.cursor()
+
+                cursor.execute("SELECT MAX (id) from imported_documents")
+                id = cursor.fetchone()[0]
+
+                cursor.execute(
+                    f"WITH country_suicides as ( "
+                    f"SELECT c.name as name, SUM(s.suicides_no) as suicides_no from suicides s, countries c "
+                    f"WHERE s.id_country=c.id "
+                    f"Group by c.name "
+                    f") "
+                    f",less_more as( "
+                    f" SELECT MIN(suicides_no) min, "
+                    f"MAX(suicides_no) max "
+                    f"    FROM country_suicides"
+                    f") "
+                    f"SELECT DISTINCT name, suicides_no "
+                    f"from country_suicides "
+                    f"WHERE suicides_no IN (select min from less_more) OR "
+                    f"suicides_no IN (select max from less_more) "
+                    f"ORDER BY suicides_no "
+                    f"limit 2 OFFSET 1 ")
+                for d in cursor:
+                    res.append(d)
+            except (Exception, psycopg2.Error) as error:
+                print("Failed to fetch data", error)
+            finally:
+                if connection:
+                    cursor.close()
+                    connection.close()
+            return res
+
 
         # signals
 

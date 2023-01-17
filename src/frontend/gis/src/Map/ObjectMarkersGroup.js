fef373735b28c720da6b4d607d9f84573650958c
Luís Filipe Malheiro <lmalheiro@ipvc.pt>
2023-01-17 21:51:15 +0000

    teste

--- src/frontend/gis/src/Map/ObjectMarkersGroup.js
+++ src/frontend/gis/src/Map/ObjectMarkersGroup.js
@@ -2,26 +2,16 @@ import React, {useEffect, useState} from 'react';
 import {LayerGroup, useMap} from 'react-leaflet';
 import {ObjectMarker} from "./ObjectMarker";
 
-
-/*
-link for icon
+/*Icon Suicides
 https://cdn-icons-png.flaticon.com/512/6349/6349523.png
  */
-
-async function requestEntities(bounds) {
-    const response = await fetch(`http://localhost:20002/api/tile/${bounds._southWest.lng}/${bounds._southWest.lat}/${bounds._northEast.lng}/${bounds._northEast.lat}`);
-    return await response.json();
-}
 
 function ObjectMarkersGroup() {
 
     const map = useMap();
-    const [geom, setGeom] = useState(...[]);
+    const [geom, setGeom] = useState([]);
     const [bounds, setBounds] = useState(map.getBounds());
 
-    /**
-     * Setup the event to update the bounds automatically
-     */
     useEffect(() => {
         const cb = () => {
             setBounds(map.getBounds());
@@ -33,18 +23,25 @@ function ObjectMarkersGroup() {
         }
     }, []);
 
-    /* Updates the data for the current bounds */
-    useEffect(async () => {
-        console.log(`> getting data for bounds`, bounds);
-        setGeom(await requestEntities(bounds));
+    useEffect(() => {
+        const {_northEast: {lat: neLat, lng: neLng}, _southWest: {lat: swLat, lng: swLng}} = bounds;
+
+
+        fetch(`http://localhost:20002/api/tile/${bounds._southWest.lng}/${bounds._southWest.lat}/${bounds._northEast.lng}/${bounds._northEast.lat}`)
+    .then(response => response.json())
+    .then(geoJSON => {
+        console.log(geoJSON);
+        setGeom(geoJSON.features)
+    })
+    .catch(err => console.log(err));
     }, [bounds])
 
     return (
         <LayerGroup>
             {
-                geom.map(geoJSON => <ObjectMarker key={geoJSON.properties.id} geoJSON={geoJSON}/>)
+                Array.isArray(geom) && geom.map(geoJSON => <ObjectMarker key={geoJSON.properties.id} position={geoJSON.properties.geometry} geoJSON={geoJSON}/>)
             }
-        </LayerGroup>
+    </LayerGroup>
     );
 }
 

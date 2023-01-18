67d0925e7f6d08418cfbdd5df2620235634531dc
Tiago Viana <tiagoviana.1003@gmail.com>
2023-01-18 18:12:19 +0000

    update front-proc

--- src/frontend/proc/src/Procedures/StatisticsPerCountry.js
+++ src/frontend/proc/src/Procedures/StatisticsPerCountry.js
@@ -1,30 +1,124 @@
 import React, {useEffect, useState} from "react";
 import {Box, CircularProgress, Container, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
-import {response} from "express";
+
+
+async function getCountries(){
+     return fetch(`http://localhost:20001/api/countries`)
+                .then(response => response.json())
+                .then(jsonData => jsonData);
+}
+const DEMO_TEAMS = [
+    {"team": "Manchester United", country: "UK"},
+    {"team": "Manchester City", country: "UK"},
+    {"team": "Chelsea", country: "UK"},
+    {"team": "Tottenham", country: "UK"},
+    {"team": "Fulham", country: "UK"},
+
+    {"team": "Sporting", country: "Portugal"},
+    {"team": "Porto", country: "Portugal"},
+    {"team": "Benfica", country: "Portugal"},
+    {"team": "Braga", country: "Portugal"},
+    {"team": "PSG", country: "France"},
+    {"team": "Lyon", country: "France"},
+    {"team": "Olympique de Marseille", country: "France"}
+];
 
 function StatisticsPerCountry() {
-  const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:20001/api/countries`)
-      .then(res => res.json())
-      .then(data => setCountries(data));
-  }, []);
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const[countries,setCoutries] = useEffect(null);
+    const [procData, setProcData] = useState(null);
+    const [gqlData, setGQLData] = useState(null);
+
+     useEffect(() => {
+        setTimeout(() => {
+            fetch(`http://localhost:20001/api/countries`)
+            .then(response => response.json())
+            .then(jsonData => {
+                debugger
+                setCoutries(jsonData)
+            });
+        }, 500);
+    }, [])
+
+    useEffect(() => {
+        //!FIXME: this is to simulate how to retrieve data from the server
+        //!FIXME: the entities server URL is available on process.env.REACT_APP_API_ENTITIES_URL
+        setProcData(null);
+        setGQLData(null);
+
+        if (selectedCountry) {
+            setTimeout(() => {
+                console.log(`fetching from ${process.env.REACT_APP_API_PROC_URL}`);
+                setProcData(DEMO_TEAMS.filter(t => t.country === selectedCountry));
+            }, 500);
+
+            setTimeout(() => {
+                console.log(`fetching from ${process.env.REACT_APP_API_GRAPHQL_URL}`);
+                setGQLData(DEMO_TEAMS.filter(t => t.country === selectedCountry));
+            }, 1000);
+        }
+    }, [selectedCountry])
 
 
-  return (
-    <div>
-      <label>Select a country:</label>
-      <select>
-        {countries.map(country => (
-          <option key={country.id} value={country.name}>
-            {country.name}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+    return (
+        <>
+            <h1>Top Teams</h1>
+
+            <Container maxWidth="100%"
+                       sx={{backgroundColor: 'background.default', padding: "2rem", borderRadius: "1rem"}}>
+                <Box>
+                    <h2 style={{color: "white"}}>Options</h2>
+                    <FormControl fullWidth>
+                        <InputLabel id="countries-select-label">Country</InputLabel>
+                        <Select
+                            labelId="countries-select-label"
+                            id="demo-simple-select"
+                            value={selectedCountry}
+                            label="Country"
+                            onChange={(e, v) => {
+                                setSelectedCountry(e.target.value)
+                            }}
+                        >
+                            <MenuItem value={""}><em>None</em></MenuItem>
+                            {
+                                countries.map(c => <MenuItem key={c.name} value={c.name}>{c.name}</MenuItem>)
+                            }
+                        </Select>
+                    </FormControl>
+                </Box>
+            </Container>
+
+            <Container maxWidth="100%" sx={{
+                backgroundColor: 'info.dark',
+                padding: "2rem",
+                marginTop: "2rem",
+                borderRadius: "1rem",
+                color: "white"
+            }}>
+                <h2>Results <small>(PROC)</small></h2>
+                {
+                    procData ?
+                        <ul>
+                            {
+                                procData.map(data => <li>{data.team}</li>)
+                            }
+                        </ul> :
+                        selectedCountry ? <CircularProgress/> : "--"
+                }
+                <h2>Results <small>(GraphQL)</small></h2>
+                {
+                    gqlData ?
+                        <ul>
+                            {
+                                gqlData.map(data => <li>{data.team}</li>)
+                            }
+                        </ul> :
+                        selectedCountry ? <CircularProgress/> : "--"
+                }
+            </Container>
+        </>
+    );
 }
-
 
 export default StatisticsPerCountry;
